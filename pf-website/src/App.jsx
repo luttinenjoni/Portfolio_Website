a6f50275998de82794ea6026f77da5bdabd3d92d
luttinenjoni <luttinenjoni@gmail.com>
2026-07-25 15:40:30 +0300

    Remove:  demo files and folders

--- pf-website/src/App.jsx
+++ pf-website/src/App.jsx
@@ -2,7 +2,10 @@ function App() {
 
   return (
     <>
+    <div>
       <h1>Welcome to my Portfolio</h1>
+      <p>This is my own portfolio website built with React.</p>
+    </div>
     </>
   )
 }

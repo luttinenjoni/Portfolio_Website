f6712e49ca5d9eb3a19dbf9deef7b304beca5764
luttinenjoni <luttinenjoni@gmail.com>
2026-07-27 15:45:28 +0300

    Create: AboutMe component and css styling

--- pf-website/src/App.jsx
+++ pf-website/src/App.jsx
@@ -1,11 +1,10 @@
+import AboutMe from "./pages/AboutMe.jsx";
+
 function App() {
 
   return (
     <>
-    <div>
-      <h1>Welcome to my Portfolio</h1>
-      <p>This is my own portfolio website built with React.</p>
-    </div>
+      <AboutMe />
     </>
   )
 }
---
+++ pf-website/src/pages/AboutMe.jsx
@@ -0,0 +1,15 @@
+import "../styles/AboutMe.css";
+
+function aboutMe() {
+  return (
+        <>    
+        <div className="about">
+            <h2>About Me</h2>
+            <p>Hello! My name is Joni Luttinen and I am a senior student at Oulu university Of Applied Sciences.</p>
+            <p>I study information technology and my main focus is on web development but I am also interested in artificial intelligence and machine learning.</p>
+        </div>
+        </>
+    )
+}
+
+export default aboutMe
